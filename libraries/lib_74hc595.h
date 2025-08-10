8a81102237131b6bb9b7c18b4ce2286beb4d2788
Joarez Fonseca <joarez.fonseca@gmail.com>
2025-08-10 08:06:17 -0300

    Update lib_74hc595.h

--- libraries/lib_74hc595.h
+++ libraries/lib_74hc595.h
@@ -11,18 +11,17 @@
 #include <stdint.h>
 #include "ch32v003fun.h"
 
-// Inicialização (define os pinos de controle)
-void HC595_Init(uint8_t data_pin, uint8_t clock_pin, uint8_t latch_pin);
+// Inicialização (agora recebe o número de CIs em cascata)
+void HC595_Init(uint8_t data_pin, uint8_t clock_pin, uint8_t latch_pin, uint8_t num_of_sr);
 
-// Escreve um byte (8 saídas) - Função original, para uso interno ou único SR
+// Escreve um byte (8 saídas)
 void HC595_WriteByte(uint8_t value);
 
-// NOVO: Escreve múltiplos bytes para 74HC595 em cascata
-// data_array: Array de bytes com os dados para cada SR. data_array[0] para o primeiro SR, data_array[1] para o segundo, etc.
-// num_of_sr: Quantidade de 74HC595 em cascata
+// Escreve múltiplos bytes para 74HC595 em cascata
 void HC595_WriteCascadedBytes(uint8_t *data_array, uint8_t num_of_sr);
 
-// Interface tipo funDigitalWrite (bit por bit) - se ainda for usada
-// void DigitalWrite(uint8_t pin, uint8_t state); // Esta não é relevante para a lib do 595
+// NOVO: Função para controlar um pino específico (0 a N*8 - 1)
+void HC595_DigitalWrite(uint8_t bitIndex, uint8_t state);
 
 #endif
+
