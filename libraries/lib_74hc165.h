bb7c431019331b170b5ef90d65eee8bfc5f1ce8f
Joarez Fonseca <joarez.fonseca@gmail.com>
2025-08-10 08:07:39 -0300

    Update lib_74hc165.h

--- libraries/lib_74hc165.h
+++ libraries/lib_74hc165.h
@@ -5,23 +5,22 @@
 //22/06/2025
 //https://youtu.be/l0zwVACMMVA
 
-#ifndef LIB_74HC595_H
-#define LIB_74HC595_H
+#ifndef LIB_74HC165_H
+#define LIB_74HC165_H
 
 #include <stdint.h>
 #include "ch32v003fun.h"
 
-// Inicialização (agora recebe o número de CIs em cascata)
-void HC595_Init(uint8_t data_pin, uint8_t clock_pin, uint8_t latch_pin, uint8_t num_of_sr);
+// Inicialização
+void HC165_Init(uint8_t data_pin, uint8_t clock_pin, uint8_t latch_pin);
 
-// Escreve um byte (8 saídas)
-void HC595_WriteByte(uint8_t value);
+// Atualiza o registrador interno com novo valor
+void HC165_Update();
 
-// Escreve múltiplos bytes para 74HC595 em cascata
-void HC595_WriteCascadedBytes(uint8_t *data_array, uint8_t num_of_sr);
-
-// NOVO: Função para controlar um pino específico (0 a N*8 - 1)
-void HC595_DigitalWrite(uint8_t bitIndex, uint8_t state);
+// Interface tipo funDigitalRead (bit por bit)
+int DigitalRead(uint8_t pin);
 
 #endif
+
+
 
