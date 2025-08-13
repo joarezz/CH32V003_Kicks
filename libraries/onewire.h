d6a39488c21d38e3bec6cd9727b342752163d4ed
Joarez Fonseca <joarez.fonseca@gmail.com>
2025-08-12 22:33:44 -0300

    Update onewire.h

--- libraries/onewire.h
+++ libraries/onewire.h
@@ -1,3 +1,9 @@
+//JOAREZ DE OLIVEIRA DA FONSECA
+//KICKS TECNOLOGIA -> SE INSCREVAM NO CANAL E NOS AJUDE A CRESCER!!!!!
+//FRAMEWORK PARA A BIBLIOTECA : CH32VFUN
+//22/06/2025
+//https://www.youtube.com/@kickstech
+
 #ifndef ONEWIRE_H
 #define ONEWIRE_H
 
@@ -13,5 +19,6 @@ uint8_t OneWire_CRC8(const uint8_t *data, uint8_t len);
 
 void OneWire_Search_Start();
 bool OneWire_Search_Next(uint8_t pin, uint8_t *newAddr);
+
 
 #endif
